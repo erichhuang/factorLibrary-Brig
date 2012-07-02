a19f9eb47cff9e352e723f65756a72a97030b9da
brig.mecham <brig.mecham@e6edf6fb-f266-4316-afb4-e53d95876a76>
2012-07-02 15:39:13 +0000

    git-svn-id: https://sagebionetworks.jira.com/svn/METAGENOMICS/trunk/factorLibrary@7998 e6edf6fb-f266-4316-afb4-e53d95876a76

--- metaFactor.R
+++ metaFactor.R
@@ -1,3 +1,4 @@
+library(lattice)
 load("betaCatenin/unsupervisedFits.Rda")
 bcat <- fits
 load("egfr/unsupervisedFits.Rda")
@@ -15,7 +16,7 @@ ras <- fits
 
 all <- list(bcat=bcat, egfr=egfr, pi3k=pi3k, src=src, e2f3=e2f3, myc=myc, ras=ras)
 gics <- sapply(all, function(x){ x[[1]]$statistics$singular.values[,1]})
-
+nms <- sapply(gics, function(x){ names(x)})
 in.common <- names(which(table(unlist(nms)) == 7))
 
 mat <- matrix(0, nr=length(in.common), ncol=length(all))
@@ -29,4 +30,10 @@ for(i in 1:length(gics)){
 obj <- 
 data.frame(fic=as.numeric(mat),
 		oncogene = rep(colnames(mat),each=nrow(mat)))
-densityplot(obj$fic, groups=obj$oncogene, plot.points=FALSE)
+png(file="allFIC.png")
+densityplot(obj$fic, 
+		groups=obj$oncogene, 
+		plot.points=FALSE,
+		auto.key=list(columns=4))
+dev.off()
+a <- paste("./add.attachments.py METAGENOMICS \"Factor Library\" \"text/plain\" ", 'allFIC.png'); system(a)
